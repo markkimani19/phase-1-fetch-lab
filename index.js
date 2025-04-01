d1ce715f055c15e0fb53bf13d9da63c0c7510b1f
mark <mark.kimani3@student.moringaschool.com>
2025-04-01 10:29:54 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
   
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json()) 
+    .then((data) => renderBooks(data)); 
 }
 
 function renderBooks(books) {
